c4b0913f80d21c5513618f52ae51ca185562f0cb
sidikriders <your@example.com>
2017-07-06 11:45:43 +0700

    baru mau bkin modal

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/foodAndRestaurant');
 var db = mongoose.connection;
@@ -16,6 +17,7 @@ db.once('open', function() {
 var index = require('./routes/index');
 
 var app = express();
+app.use(cors())
 
 
 // uncomment after placing your favicon in /public
@@ -27,7 +29,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
